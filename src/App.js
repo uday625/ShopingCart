8565580a6197bcf6e6baf6cf7ef7a7fbdd61ccb9
UDAY BAG <uday625@Nemesis-2.local>
2018-09-16 23:45:39 -0400

    adds reset button

--- src/App.js
+++ src/App.js
@@ -15,15 +15,25 @@ class App extends Component {
   }
 
   deleteHandler = (counterId) => {
-    console.log("Delete Handler called", counterId);
-    // const counters = [...this.state.counters];
-    // counters.splice(counterId,1);
-
     const counters = this.state.counters.filter(c => c.id !==counterId)
-
     this.setState({counters:counters});
   }
 
+  incrementHandler = (counter) =>{
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = {...counter}
+    counters[index].value ++;
+    this.setState({counters:counters});
+  }
+
+  resetHandler = () =>{
+    const counters = this.state.counters.map(counter =>{
+      counter.value =0
+      return counter;
+    })
+    this.setState({counters:counters});
+  }
 
   render() {
     return (
@@ -33,14 +43,18 @@ class App extends Component {
           {this.state.counters.map(counter => 
           <Counter 
             key ={counter.id} 
-            onDelete = {this.deleteHandler}  
+            onDelete = {this.deleteHandler} 
+            onIncrement = {this.incrementHandler} 
             counter = {counter}
             />)}
+          <button 
+            className="btn btn-info btn-sm m-2"
+            onClick ={this.resetHandler}>
+            Reset
+          </button>
       </div>
     );
   }
-
-
 }
 
 export default App;
